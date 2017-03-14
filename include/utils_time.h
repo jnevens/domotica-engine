7fcabfec5f1bc0fe0d17dd7691fe7459c3af1eb4
Jasper Nevens <jasper.nevens@linux.com>
2017-03-14 21:26:49 +0100

    add time utilities

--- include/utils_time.h
+++ include/utils_time.h
@@ -13,4 +13,11 @@
 uint64_t get_current_time_ms(void);
 uint64_t get_current_time_us(void);
 
+// Busy wait delay for most accurate timing, but high CPU usage.
+// Only use this for short periods of time (a few hundred milliseconds at most)!
+void busy_wait_milliseconds(uint32_t millis);
+
+// General delay that sleeps so CPU usage is low, but accuracy is potentially bad.
+void sleep_milliseconds(uint32_t millis);
+
 #endif /* UTILS_TIME_H_ */
