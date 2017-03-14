b0fa0b6b0acdfdeb039e290c987d396d613b4a08
Jasper Nevens <jasper.nevens@linux.com>
2017-03-14 21:23:02 +0100

    fsb14: add inittial support for eltako blinds

---
+++ include/eltako_fsb14.h
@@ -0,0 +1,15 @@
+/*
+ * eltako_fsb14.h
+ *
+ *  Created on: Mar 14, 2017
+ *      Author: jnevens
+ */
+
+#ifndef ELTAKO_FSB14_H_
+#define ELTAKO_FSB14_H_
+
+#include <stdbool.h>
+
+bool eltako_fsb14_init(void);
+
+#endif /* ELTAKO_FSB14_H_ */
