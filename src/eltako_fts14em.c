80cce347f31813e4722c930d45eabf7e5b256d66
Jasper Nevens <jasper.nevens@linux.com>
2017-09-17 17:59:05 +0200

    fts14em: remove DIM event, fix segfault

--- src/eltako_fts14em.c
+++ src/eltako_fts14em.c
@@ -22,7 +22,6 @@
 typedef struct {
 	uint32_t address;
 	uint64_t press_time;
-	eu_event_timer_t *timer;
 } fts14em_device_t;
 
 static bool fts14em_device_parser(device_t *device, char *options[])
@@ -37,27 +36,6 @@ static bool fts14em_device_parser(device_t *device, char *options[])
 	return false;
 }
 
-bool eltako_fts14em_dim_press_timer_cb(void *arg)
-{
-	device_t *device = arg;
-
-	device_trigger_event(device, EVENT_DIM);
-
-	return eu_event_timer_continue;
-}
-
-bool eltako_fts14em_short_press_timer_cb(void *arg)
-{
-	device_t *device = arg;
-	fts14em_device_t *fts14em_device = device_get_userdata(device);
-
-	fts14em_device->timer = eu_event_timer_create(PRESS_DIM_INTERVAL_TIME_MS, eltako_fts14em_dim_press_timer_cb, device);
-
-	device_trigger_event(device, EVENT_DIM);
-
-	return eu_event_timer_stop;
-}
-
 void eltako_fts14em_incoming_data(eltako_message_t *msg, device_t *device)
 {
 	int status = eltako_message_get_status(msg);
@@ -66,26 +44,23 @@ void eltako_fts14em_incoming_data(eltako_message_t *msg, device_t *device)
 	if(status == 0x30) { // press
 		device_trigger_event(device, EVENT_PRESS);
 		fts14em_device->press_time = get_current_time_ms();
-
-		if(fts14em_device->timer) {
-			eu_event_timer_destroy(fts14em_device->timer);
-		}
-		fts14em_device->timer = eu_event_timer_create(SHORT_PRESS_MAX_TIME_MS, eltako_fts14em_short_press_timer_cb, device);
 	} else if (status == 0x20) { // release
 		uint64_t press_time = fts14em_device->press_time;
-		uint64_t release_time = get_current_time_ms();
-		uint64_t press_duration = release_time - press_time;
-		eu_log_debug("press duration: %llums", press_duration);
+		if (press_time > 0) {
+			uint64_t release_time = get_current_time_ms();
+			uint64_t press_duration = release_time - press_time;
+			eu_log_debug("press duration: %llums", press_duration);
 
-		device_trigger_event(device, EVENT_RELEASE);
+			device_trigger_event(device, EVENT_RELEASE);
+			fts14em_device->press_time = 0;
 
-		eu_event_timer_destroy(fts14em_device->timer);
-		fts14em_device->timer = NULL;
-
-		if(press_duration <= SHORT_PRESS_MAX_TIME_MS) {
-			device_trigger_event(device, EVENT_SHORT_PRESS);
+			if (press_duration <= SHORT_PRESS_MAX_TIME_MS) {
+				device_trigger_event(device, EVENT_SHORT_PRESS);
+			} else {
+				device_trigger_event(device, EVENT_LONG_PRESS);
+			}
 		} else {
-			device_trigger_event(device, EVENT_LONG_PRESS);
+			eu_log_warn("Ignoring release event without press time! %s", device_get_name(device));
 		}
 	}
 }
@@ -103,7 +78,7 @@ uint32_t eltako_fts14em_get_address(device_t *device)
 
 static device_type_info_t fts14em_info = {
 	.name = "FTS14EM",
-	.events = EVENT_PRESS | EVENT_RELEASE | EVENT_SHORT_PRESS | EVENT_LONG_PRESS | EVENT_DIM,
+	.events = EVENT_PRESS | EVENT_RELEASE | EVENT_SHORT_PRESS | EVENT_LONG_PRESS,
 	.actions = 0,
 	.conditions = 0,
 	.check_cb = NULL,
